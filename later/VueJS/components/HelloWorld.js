78985146b595a4cd4733296c6b25680b8089b623
Asbach <andre.simon.amakunai@googlemail.com>
2019-11-08 22:29:14 +0100

    somthingtocommit

--- later/VueJS/components/HelloWorld.js
+++
@@ -1,9 +0,0 @@
-new Vue({
-    el: '#helloworld',
-    data: () => {
-        return {
-            message: 'Hello World!',
-            something: 'Ich bin eine weitere property',
-        }
-    }
-  })
